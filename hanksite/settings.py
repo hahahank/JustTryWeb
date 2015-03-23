6223988637ee73a0c62f530eda33f4d8e044ae59
hahahank <chang330@gmail.com>
2015-03-23 16:01:04 +0800

    templates setting

--- hanksite/settings.py
+++ hanksite/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'urls'
+
+
+"""  Templates Settings  """
 '''
 TEMPLATES = [
     {
@@ -68,11 +71,31 @@ TEMPLATES = [
             ],
         },
     },
-]'''
+]
+'''
 TEMPLATE_DIRS = (
     'templates',
     os.path.join(SETTINGS_PATH, ''),
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+
+
+
 
 
 WSGI_APPLICATION = 'wsgi.application'
