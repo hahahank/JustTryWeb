90962335476e1dbfd2004b63f1b427f2fc547480
hahahank <chang330@gmail.com>
2015-03-23 15:31:02 +0800

    urls and pages 2

--- hanksite/urls.py
+++ hanksite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url,patterns
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = patterns('', 
     # home
     (r'^$', 'main.views.login'),
