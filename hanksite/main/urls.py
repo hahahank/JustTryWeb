3a92cb9a729c0d3391e2e55bc1c4882159024a45
hahahank <chang330@gmail.com>
2015-03-23 21:19:03 +0800

    aasd

--- hanksite/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url,patterns
-from django.contrib import admin
-from django.conf import settings
-
-urlpatterns = patterns('',
-    url(r'^index/$', 'main.views.index'),                         
-  
-)
