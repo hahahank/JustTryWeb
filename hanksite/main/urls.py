689e5d63eaed4e7e87c3dbe664ff2eb4e5ac8f53
hahahank <chang330@gmail.com>
2015-03-23 15:49:29 +0800

    aa

--- hanksite/main/urls.py
+++ hanksite/main/urls.py
@@ -4,11 +4,5 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^index/$', 'main.views.index'),                         
-#    url(r'^heading/$', 'main.views.heading'),
-    url(r'^footing/$', 'main.views.content'),    
-    url(r'^menu/$', 'main.views.menu'),
-    url(r'^bar/$', 'main.views.bar'),
-    url(r'^content/$', 'main.views.content'),
-    url(r'^logout/$', 'main.views.logout'),
-    url(r'^root/$', 'main.views.root'),        
+  
 )
--- hanksite/main/views.py
+++ hanksite/main/views.py
@@ -1,11 +1,13 @@
 # Main page redirect
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse
+
 # Redirect to login page
 def login(request):
-    return HttpResponse("Hello, world. You're at the polls index.!!!!!!!!!")
-
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    c = {}
+    c.update(csrf(request))    
+    return render_to_response('templates/login.html', c)
 
 # Redirect to logout page
 def logout(request):    
@@ -13,4 +15,5 @@ def logout(request):
 
 # Redirect to index page
 def index(request):    
+    print "index "
     return render_to_response('templates/main/index.html')
