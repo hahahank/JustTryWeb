fc6de3f62e44d7e8c83e07f55b3bd50caee31388
hahahank <chang330@gmail.com>
2015-03-23 15:43:19 +0800

    0343

--- hanksite/main/views.py
+++ hanksite/main/views.py
@@ -1,12 +1,11 @@
 # Main page redirect
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, get_object_or_404
-
+from django.http import HttpResponse
 # Redirect to login page
 def login(request):
-    c = {}
-    c.update(csrf(request))    
-    return render_to_response('templates/login.html', c)
+    return HttpResponse("Hello, world. You're at the polls index.!!!!!!!!!")
+
 
 # Redirect to logout page
 def logout(request):    
