96f040f399880ae4ebcc5bedce95d27eac1bb748
hahahank <chang330@gmail.com>
2015-03-23 15:03:20 +0800

    03230303

--- hanksite/wsgi.py
+++ hanksite/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', ".."))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
